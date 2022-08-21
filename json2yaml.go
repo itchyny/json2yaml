7e9a158b36b3bfcb8cf4d565195d8dce92a58972
itchyny <itchyny@cybozu.co.jp>
2022-08-21 16:36:11 +0900

    add Convert function

--- json2yaml.go
+++ json2yaml.go
@@ -1,2 +1,10 @@
 // Package json2yaml implements a converter from JSON to YAML.
 package json2yaml
+
+import "io"
+
+// Convert reads JSON from r and writes YAML to w.
+func Convert(w io.Writer, r io.Reader) error {
+	io.Copy(w, r)
+	return nil
+}
---
+++ json2yaml_test.go
@@ -0,0 +1,49 @@
+package json2yaml_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/itchyny/json2yaml"
+)
+
+func TestConvert(t *testing.T) {
+	testCases := []struct {
+		name string
+		src  string
+		want string
+		err  string
+	}{
+		{
+			name: "empty object",
+			src:  "{}",
+			want: "{}",
+		},
+		{
+			name: "empty array",
+			src:  "[]",
+			want: "[]",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var sb strings.Builder
+			err := json2yaml.Convert(&sb, strings.NewReader(tc.src))
+			if tc.err == "" {
+				if err != nil {
+					t.Fatalf("should not raise an error but got: %s", err)
+				}
+				if got := sb.String(); got != tc.want {
+					t.Fatalf("should write\n  %q\nbut got\n  %q\nwhen source is\n  %q", tc.want, got, tc.src)
+				}
+			} else {
+				if err == nil {
+					t.Fatalf("should raise an error %s but got no error", tc.err)
+				}
+				if !strings.Contains(err.Error(), tc.err) {
+					t.Fatalf("should raise an error %s but got error %s", tc.err, err)
+				}
+			}
+		})
+	}
+}
