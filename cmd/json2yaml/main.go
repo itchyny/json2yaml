4854f4637377195d511cbdd8165b80a55bb9c5df
itchyny <itchyny@cybozu.co.jp>
2022-08-24 22:10:27 +0900

    implement json2yaml command line tool

--- cmd/json2yaml/main.go
+++ cmd/json2yaml/main.go
@@ -1,7 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/itchyny/json2yaml"
+)
+
+const name = "json2yaml"
+
+const version = "0.0.0"
+
+var revision = "HEAD"
 
 func main() {
-	fmt.Println("json2yaml")
+	os.Exit(run(os.Args[1:]))
+}
+
+const (
+	exitCodeOK = iota
+	exitCodeErr
+)
+
+func run(args []string) (exitCode int) {
+	fs := flag.NewFlagSet(name, flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
+	fs.Usage = func() {
+		fs.SetOutput(os.Stdout)
+		fmt.Printf(`%[1]s - convert JSON to YAML
+
+Version: %s (rev: %s/%s)
+
+Synopsis:
+  %% %[1]s file ...
+
+Options:
+`, name, version, revision, runtime.Version())
+		fs.PrintDefaults()
+	}
+	var showVersion bool
+	fs.BoolVar(&showVersion, "version", false, "print version")
+	if err := fs.Parse(args); err != nil {
+		if err == flag.ErrHelp {
+			return exitCodeOK
+		}
+		return exitCodeErr
+	}
+	if showVersion {
+		fmt.Printf("%s %s (rev: %s/%s)\n", name, version, revision, runtime.Version())
+		return exitCodeOK
+	}
+	if args = fs.Args(); len(args) == 0 {
+		if err := convert("-"); err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", name, err)
+			exitCode = exitCodeErr
+		}
+	} else {
+		for i, arg := range args {
+			if i > 0 {
+				fmt.Fprintln(os.Stdout, "---")
+			}
+			if err := convert(arg); err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %s\n", name, err)
+				jerr := &json.SyntaxError{}
+				if errors.As(err, &jerr) {
+					// Clear incomplete output for next argument.
+					fmt.Fprintln(os.Stdout)
+				}
+				exitCode = exitCodeErr
+			}
+		}
+	}
+	return
+}
+
+func convert(name string) error {
+	if name == "-" {
+		if err := json2yaml.Convert(os.Stdout, os.Stdin); err != nil {
+			return fmt.Errorf("<stdin>: %w", err)
+		}
+		return nil
+	}
+	f, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if err := json2yaml.Convert(os.Stdout, f); err != nil {
+		return fmt.Errorf("%s: %w", name, err)
+	}
+	return nil
 }
